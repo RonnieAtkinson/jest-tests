4800756c248acd06314260a69e68f5ec121b9436
Ronnie Atkinson <1983roncode@gmail.com>
2021-07-25 19:07:07 +0100

    Removed sanity check

--- sanity.testl.js
+++
@@ -1,7 +0,0 @@
-describe('Sanity check', () => {
-    describe('does not throw an error', () => {
-        test('true equals true', () => {
-            expect(true).toBe(true);
-        });
-    });
-});
